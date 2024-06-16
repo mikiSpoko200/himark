7ee680b70d993f45814384068f697d8db5a9200a
mikiSpoko200 <328690@uwr.edu.pl>
2024-06-16 16:14:37 +0200

    Add support for generic parameters and enums

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,12 @@
 extern crate proc_macro;
 
 use proc_macro::TokenStream;
-use quote::{quote, quote_spanned, TokenStreamExt};
+use quote::{quote, quote_spanned};
 use syn::{parse_macro_input, ItemTrait};
 
 #[proc_macro_attribute]
 /// Attribute for use on traits which verifies that they are indeed markers.
-/// 
+///
 /// Trait is considered a marker if:
 /// - has no associated items
 /// - all its super traits are also markers or auto traits
@@ -15,9 +15,12 @@ pub fn marker(_: TokenStream, input: TokenStream) -> TokenStream {
     let input_trait = parse_macro_input!(input as ItemTrait);
 
     if !input_trait.items.is_empty() {
-        return syn::Error::new_spanned(input_trait, "The #[marker] attribute can only be applied to empty traits")
-            .to_compile_error()
-            .into();
+        return syn::Error::new_spanned(
+            input_trait,
+            "The #[marker] attribute can only be applied to empty traits",
+        )
+        .to_compile_error()
+        .into();
     }
 
     let gen = quote! {
@@ -27,36 +30,37 @@ pub fn marker(_: TokenStream, input: TokenStream) -> TokenStream {
     gen.into()
 }
 
-
 #[proc_macro_attribute]
 /// Attribute for marking types with marker traits.
 pub fn mark(args: TokenStream, input: TokenStream) -> TokenStream {
-    let parsed_input = parse_macro_input!(input as syn::ItemStruct);
+    let parsed_input = parse_macro_input!(input as syn::DeriveInput);
 
-    let mut triat_names = Vec::new();
+    let mut trait_names = Vec::new();
 
-    let parser = syn::meta::parser(|meta| 
-        match meta.value() {
-            Err(_) => {
-                triat_names.push(meta.path);
-                Ok(())
-            },
-            Ok(_) => Err(meta.error("expected trait name"))
+    // Parser to collect the trait names from the attribute arguments
+    let parser = syn::meta::parser(|meta| match meta.value() {
+        Err(_) => {
+            trait_names.push(meta.path);
+            Ok(())
         }
-    );
+        Ok(_) => Err(meta.error("expected trait name")),
+    });
 
     parse_macro_input!(args with parser);
 
-    let impls = triat_names.iter()
-        .map(|trait_name| {
-            let ident = &parsed_input.ident;
-            let span = parsed_input.ident.span();
-            quote_spanned! {span=>
-                impl #trait_name for #ident { }
-            }
-        }); 
+    let ident = &parsed_input.ident;
+    let span = parsed_input.ident.span();
+    let generics = &parsed_input.generics;
+    let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
+
+    let impls = trait_names.iter().map(|trait_name| {
+        quote_spanned! {span=>
+            impl #impl_generics #trait_name for #ident #ty_generics #where_clause {}
+        }
+    });
 
     let mut gen = quote::quote! { #parsed_input };
-    gen.append_all(impls);
+    gen.extend(impls);
+
     gen.into()
 }
