60b4de71cad50799fdff6edf2e93023225d30d6f
mikiSpoko200 <328690@uwr.edu.pl>
2024-06-16 17:45:37 +0200

    Fix module level docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,5 @@
 //! Oh, hi `mark`.
 //! 
-//! [github]: https://img.shields.io/badge/github-8da0cb?style=for-the-badge&labelColor=555555&logo=github
-//! [crates-io]: https://img.shields.io/badge/crates.io-fc8d62?style=for-the-badge&labelColor=555555&logo=rust
-//! [docs-rs]: https://img.shields.io/badge/docs.rs-66c2a5?style=for-the-badge&labelColor=555555&logo=docs.rs
-//! 
 //! For those who crave more ergonomic marker traits.
 //! 
 //! ## Introduction
